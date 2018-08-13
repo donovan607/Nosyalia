277593a6ca7626ac9126646032032c9d4830173c
donovan607 <42345060+donovan607@users.noreply.github.com>
2018-08-14 01:31:58 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+
+//Connexion et set de l'activité
+bot.on('ready', function () {
+    console.log(`Connexion effectué !`);
+    bot.user.setActivity('Nosyalia !')
+});
+
+//Message Ping/Pong
+bot.on('message', msg => {
+    if (msg.content === '!ping') {
+        msg.reply('Pong!');
+    }
+});
+
+//Message ip du serveur
+bot.on('message', msg => {
+    if (msg.content === '!ip') {
+        msg.channel.send('IP du serveur minecraft Nosyalia :' + '\n' + '```markdown\n' + 'play.nosyalia.tk\n' + '```' + 'Rejoins nous dès maintenant !');
+    }
+});
+
+//Login du bot sur le serveur
+bot.login('NDc4NTMzNTgwMzg2MDc0NjI0.DlNp8g.lF7rD_t8o96V2g7VVoEwbPPUKI0')
