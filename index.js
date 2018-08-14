612d69dfe38dca57c06d999a13e91d490e91a056
donovan607 <42345060+donovan607@users.noreply.github.com>
2018-08-14 22:03:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,3 +24,15 @@ bot.on('message', msg => {
 
 //Login du bot sur le serveur
  bot.login(process.env.TOKEN)
+
+///Embed
+const ce = require("embed-creator");
+bot.on('message', msg => {
+    if (msg.content === '!bvn') {
+    msg.channel.send(ce(
+        "#FEAFEA", {"name": "Fire", "icon_url": msg.author.displayAvatarURL(), "url": "https://www.google.com"}, "Title", "Description",
+        [{"name": "Field 1", "value": "Value 1"},
+        {"name": "Field 2", "value": "Value 2"}],
+        {"text": "This is footer text!", "icon_url": msg.guild.iconURL()},
+        {"thumbnail": msg.guild.iconURL(), "image": msg.author.displayAvatarURL()}, false
+}});
